cba2b6f7263c4c4a6b0aa3f2de052de62ab741b4
0xClint <omkarbdarde@gmail.com>
2023-12-14 18:25:10 +0530

    intro to pointers

---
+++ 5-Pointers/pointers-part-1.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int num = 5;
+
+    int *ptr = &num;
+
+    cout << "value of num = " << num << endl;
+    cout << "size of num = " << sizeof(num) << endl;
+    cout << "address of num = " << &num << endl;
+    cout << "value of ptr = " << ptr << endl;
+    cout << "size of ptr = " << sizeof(ptr) << endl;
+    cout << "value of *ptr = " << *ptr << endl;
+    (*ptr)++;
+    cout << "value of num = " << num << endl;
+    int *qtr = ptr;
+    cout << "ptr = " << ptr << " and "
+         << " qtr = " << qtr << endl;
+    cout << "*ptr = " << *ptr << " and "
+         << " *qtr = " << *qtr << endl;
+
+    cout << "value of ptr = " << ptr << endl;
+    ptr++;
+    cout << "value of ptr = " << ptr << endl;
+}
